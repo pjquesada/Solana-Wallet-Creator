ce444ec63f1c106f400a42db9b3969772226ba7a
Pablo Quesada <89797292+pjquesada@users.noreply.github.com>
2022-03-28 13:42:52 -0400

    Uploading Files

---
+++ utils/index.ts
@@ -0,0 +1,44 @@
+// Import any additional classes and/or functions needed from Solana's web3.js library as you go along:
+import { Cluster, clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { message } from "antd";
+
+// *Step 3*: implement a function that gets an account's balance
+const refreshBalance = async (network: Cluster | undefined, account: Keypair | null) => {
+  // This line ensures the function returns before running if no account has been set
+  if (!account) return;
+
+  try {
+    const connection = new Connection(clusterApiUrl(network), "confirmed");
+    const publicKey = account.publicKey;
+    const balance = await connection.getBalance(publicKey);
+
+    return balance / LAMPORTS_PER_SOL;
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown Error";
+    message.error(`Balance refresh failed: ${errorMessage}`);
+    return 0;
+  }
+};
+
+// *Step 4*: implement a function that airdrops SOL into devnet account
+const handleAirdrop = async (network: Cluster, account: Keypair | null) => {
+  if (!account) return;
+
+  try {
+    const connection = new Connection(clusterApiUrl(network), "confirmed");
+    const publicKey = account.publicKey;
+    const confirmation = await connection.requestAirdrop(
+        publicKey,
+        LAMPORTS_PER_SOL
+    );
+    const result = await connection.confirmTransaction(confirmation, "confirmed");
+    return await refreshBalance(network, account);
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown Error";
+    message.error(`Airdrop failed: ${errorMessage}`);
+  }
+};
+
+export { refreshBalance, handleAirdrop };
